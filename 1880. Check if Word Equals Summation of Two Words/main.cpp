4d6e91b741bbe71b92359e13593ed185602224f8
Mikhail Y. Dmitrienko <dmitrienko@sl.iae.nsk.su>
2021-06-11 13:38:09 +0700

    task 128 fix

--- 1880. Check if Word Equals Summation of Two Words/main.cpp
+++ 1880. Check if Word Equals Summation of Two Words/main.cpp
@@ -1,14 +1,14 @@
-#include "Tasks.h"
+#include "../1880. Check if Word Equals Summation of Two Words/Tasks.ñpp"
 #include <iostream>
 #include <cassert>
 int main()
 {
    /* std::vector<int> param1{ 100,4,200,1,3,2 };
     assert( task128::Solution::longestConsecutive(param1) == 4);*/
-    std::vector<int> param2{ 1,2,0,1 };
-    assert(task128::Solution::longestConsecutive(param2) == 3);
+    /*std::vector<int> param2{ 1,2,0,1 };
+    assert(task128::Solution::longestConsecutive(param2) == 3);*/
     std::vector<int> param3{ 0,3,7,2,5,8,4,6,0,1 };
-    assert(task128::Solution::longestConsecutive(param2) == 9);
+    assert(task128::Solution::longestConsecutive(param3) == 9);
     
     
 
